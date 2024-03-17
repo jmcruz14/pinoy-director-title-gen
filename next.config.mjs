e8f5035d1bb29698a3072cb9210cc71787d95178
Jay <jccruz009@yahoo.com>
2024-03-17 16:29:03 +0800

    chore: enable EsmExternals referencing
    chore: install additional dependency (open)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: 'loose' // find more stable method of importing esm functions
+  }
+};
 
 export default nextConfig;
